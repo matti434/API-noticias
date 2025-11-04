0bbb98a52bae6b7f551b31162e0e8a332024c8fb
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-11-03 21:20:11 -0300

    feat: Estructura basica

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Componentes/views/Home.jsx";
+import { Menu } from "./Componentes/shared/Menu.jsx";
+import { Footer } from "./Componentes/shared/Footer.jsx";
+import { BrowserRouter,Routes,Route } from "react-router";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <BrowserRouter>
+        <Menu/>
+        <main>
+          <Routes>
+          <Route path="" element={<Home/>}></Route>
+          </Routes>
+        </main>
+        <Footer />
+     </BrowserRouter>  
     </>
-  )
+  );
 }
 
-export default App
+export default App;
