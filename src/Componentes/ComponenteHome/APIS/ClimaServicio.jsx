d2e67ec1d0f7ff66544478edf1dc63f31ec1d0fb
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-11-05 16:17:03 -0300

    api terminada

--- src/Componentes/ComponenteHome/APIS/ClimaServicio.jsx
+++ src/Componentes/ComponenteHome/APIS/ClimaServicio.jsx
@@ -1,11 +1,15 @@
 export const ClimaServicio = async (latitud,longitud) => {
   
-    
-    const respuesta = await fetch( `https://api.open-meteo.com/v1/forecast?latitude=-34&longitude=-64&hourly=temperature_2m&timezone=America%2FSao_Paulo`);
+    /*
+    const respuesta = await fetch( `https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&hourly=temperature_2m&timezone=America%2FSao_Paulo`);
+    */
 
+    const respuesta = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&current=temperature_2m&timezone=auto`);
+    
     if(!respuesta){
         throw new Error("Datos no recibidos");
     }
+
 
     const data = await respuesta.json();
     return data;
